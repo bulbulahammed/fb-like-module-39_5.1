a8ddd2410f4364d9891e9f3f8a07f3b7a93f4e8f
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2021-03-12 21:31:51 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { AccessAlarm, ThreeDRotation, ThumbUpIcon } from '@material-ui/icons';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import { useState } from 'react';
+
 
 function App() {
+  const [likeColor, setLikeColor] = useState('');
+  const handleLike = () => {
+    const color = likeColor ? '' : 'primary';
+    setLikeColor(color);
+  }
   return (
     <div className="App">
+      <AccessAlarm></AccessAlarm>
+      <ThreeDRotation></ThreeDRotation>
+      <ThumbUpAltIcon onClick={handleLike} color={likeColor}></ThumbUpAltIcon>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
